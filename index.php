3dafc5c9cf7afed46b86cad658f8e06a3bc07e81
Duc Tan Dang <ductan.dang@googlemail.com>
2018-06-16 15:17:13 +0200

    feat(index php): add content

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php 
-    phpinfo();
-?>
+<!doctype html>
+<html>
+  <link>
+    <title>Bhuddies</title>
+    <link rel="stylesheet" href="dist/main.css">
+  </head>
+  <body>
+
+    <h1>hello world</h1>
+    <p>some parapgraphs</p>
+
+
+    <script src="./dist/main.js"></script>
+  </body>
+</html>
