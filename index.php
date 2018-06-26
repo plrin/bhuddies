1a418d0cbfe34cbf52b8ab2051236e2300c4730a
Duc Tan Dang <ductan.dang@googlemail.com>
2018-06-26 23:50:19 +0200

    fix(chore): add markup to php file

--- index.php
+++ index.php
@@ -1,15 +1,45 @@
 <!doctype html>
 <html>
-  <link>
-    <title>Bhuddies</title>
-    <link rel="stylesheet" href="dist/main.css">
-  </head>
-  <body>
+<link>
+<title>Bhuddies</title>
+<link rel="stylesheet" href="dist/main.css">
+</head>
 
-    <h1>hello world</h1>
-    <p>some parapgraphs</p>
+<body>
+
+  <header class="header">
+    <h1>Hi meine Buddhies, please subscibe here:</h1>
+  </header>
+
+  <!-- Begin MailChimp Signup Form -->
+  <div id="mc_embed_signup" class="form">
+    <form action="https://buddhies.us18.list-manage.com/subscribe/post?u=5d0fe5dfc4144a3292dcd07d2&amp;id=185b2687c8" method="post"
+      id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+      <div id="mc_embed_signup_scroll">
+
+        <div class="mc-field-group">
+          <!-- <label for="mce-EMAIL">Email Address</label> -->
+          <input type="email" value="" name="EMAIL" class="required email form__input" id="mce-EMAIL">
+        </div>
+        <div id="mce-responses" class="clear">
+          <div class="response" id="mce-error-response" style="display:none"></div>
+          <div class="response" id="mce-success-response" style="display:none"></div>
+        </div>
+        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px;" aria-hidden="true">
+          <input type="text" name="b_5d0fe5dfc4144a3292dcd07d2_185b2687c8" tabindex="-1" value="">
+        </div>
+        <div class="clear">
+          <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button form__submit">
+        </div>
+      </div>
+    </form>
+  </div>
+
+  <!--End mc_embed_signup-->
 
 
-    <script src="./dist/main.js"></script>
-  </body>
+  <script src="./dist/main.js"></script>
+</body>
+
 </html>
